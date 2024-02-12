3de9adfae012361801503df85090c29dcf7fa739
mufaqar <mufaqar@gmail.com>
2024-02-12 18:06:54 +0500

    number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-
 
 export default function Home() {
   return (
@@ -14,7 +11,7 @@ export default function Home() {
           offers one of the most professional car services
         </p>
 
-        <iframe src="https://billink.no/page2_iframe.php?butikk=harris_auto" width="100%" height="3380" frameBorder="0" marginWidth="0" marginheight="0" scrolling="auto" align="middle"></iframe>
+        <iframe src="https://billink.no/page2_iframe.php?butikk=harris_auto" width="100%" height="3380"  ></iframe>
 
       </div>
 
